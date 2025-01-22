bfcf7c4c82bb9533d1ea4f2cb88da6d6539672ae
eslam <eslamdeveloper1@gmail.com>
2025-01-22 19:30:58 +0200

    a

--- js/sales.js
+++ js/sales.js
@@ -24,3 +24,43 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+
+
+// دالة لإضافة حقل جديد فى اضافة دورات شهرية 
+function addNewField() {
+    const container = document.getElementById('time-of-sending-fields');
+
+    // إنشاء الحقل الجديد
+    const newField = document.createElement('div');
+    newField.classList.add('row', 'mb-3');
+
+    newField.innerHTML = `
+        <div class="col-md-6">
+            <div class="form-group">
+                <select class="form-control">
+                    <option selected disabled>اختر متى</option>
+                    <option>فى تاريخ الاصدار</option>
+                    <option>فى تاريخ الفاتورة</option>
+                    <option>فى تاريخ الاستحقاق</option>
+                    <option>قبل ميعاد الفاتورة (عدد أيام)</option>
+                    <option>بعد ميعاد الفاتورة (عدد أيام)</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <button class="btn btn-icon text-danger" onclick="removeField(this)">
+                <i class="bi bi-trash"></i>
+            </button>
+        </div>
+    `;
+
+    // إضافة الحقل الجديد إلى القسم
+    container.appendChild(newField);
+}
+
+// دالة لحذف الحقل
+function removeField(button) {
+    const fieldRow = button.closest('.row');
+    fieldRow.remove();
+}
